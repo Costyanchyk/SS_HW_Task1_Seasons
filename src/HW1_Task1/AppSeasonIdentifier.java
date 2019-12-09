15d1e9bfa901ece321dc77394c3fbfb78576d34d
kpavle <pavlenko.k@hotmail.com>
2019-12-09 13:17:07 +0200

    HW1_Task1 Application for Identifying the season

--- src/HW1_Task1/AppSeasonIdentifier.java
+++ src/HW1_Task1/AppSeasonIdentifier.java
@@ -3,18 +3,77 @@ package HW1_Task1;
 import java.util.Scanner;
 
 public class AppSeasonIdentifier {
+    static String currentSeason;
+    static boolean starter = true;
+
     public static void main(String[] args) {
-        RecognitionSeasonMethod m = new RecognitionSeasonMethod();
-        System.out.println("What is the current month ?");
 
+        AppSeasonIdentifier objectName = new AppSeasonIdentifier();
+        while (starter) {
+            objectName.questionToContinue();
+        }
+    }
+
+    // scanner + Swift
+    public String identifierSeason() {
+        System.out.println("What is the current month ?\n");
         Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        String modMonth = text.toLowerCase(); //create transit variable
+        String month = scanner.nextLine().toLowerCase();
 
-        System.out.println(m.IdentSeason(modMonth));
-        scanner.close();
+        switch (month) {
+            case "december":
+            case "january":
+            case "february":
+                System.out.println("This is " + ListOfSeasons.WINTER + " buddy!!!\n");
+                break;
+            case "june":
+            case "july":
+            case "august":
+                System.out.println("This is " + ListOfSeasons.SUMMER + " buddy!!!\n");
+                break;
+            case "september":
+            case "october":
+            case "november":
+                System.out.println("This is " + ListOfSeasons.AUTUMN + " buddy!!!\n");
+                break;
+            case "march":
+            case "april":
+            case "may":
+                System.out.println("This is " + ListOfSeasons.SPRING + " buddy!!!\n");
+                break;
+            default:
+                System.out.println("Please write correct month buddy!!!\n");
+                identifierSeason();
+        }
+        return currentSeason;
+    }
+
+
+    // question "Do you want to continue"
+    public void questionToContinue() {
+        System.out.println("This app helps to identify the season.\nDo you want to continue? <y/n>");
+        Scanner scanner = new Scanner(System.in);
+        String response = scanner.nextLine();
+        if (response.equalsIgnoreCase("y") ||
+                response.equalsIgnoreCase("yes") ||
+                response.equalsIgnoreCase("ye") ||
+                response.equalsIgnoreCase("yeah") ||
+                response.equalsIgnoreCase("+") ||
+                response.equalsIgnoreCase("yea")) {
+            identifierSeason();
+        } else {
+            System.out.println("Thank you for using my App!!");
+            starter = false;
+        }
+    }
+
+
+    // list of seasons
+    public enum ListOfSeasons {
+        WINTER, AUTUMN, SPRING, SUMMER
     }
 }
+
 
 
 
--- src/HW1_Task2/MinMaxValues.java
+++ src/HW1_Task2/MinMaxValues.java
@@ -5,71 +5,79 @@ import java.util.Scanner;
 public class MinMaxValues {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the necessary type of variable\n (for example: int, char, bool)\n Type <all> command to show result table\n Type <info> for more information");
+        System.out.println("Привет как тебя зовут?"); //pe of variable\n (for example: int, char, bool)\n Type <all> command to show result table\n Type <info> for more information");
         String type = scanner.nextLine();
         String modType = type.toLowerCase(); //create transit variable for LowerCase transformation
-
-        switch (modType) {
-            case "info":
-                System.out.println("Hello, this program helps to display max and min values of different type of the variables");
-                break;
-            case "int":
-            case "integer":
-                System.out.println("The MAX value is:" + Integer.MAX_VALUE);
-                System.out.println("The MIN value is:" + Integer.MIN_VALUE);
-                break;
-            case "byte":
-                System.out.println("The MAX value is:" + Byte.MAX_VALUE);
-                System.out.println("The MIN value is:" + Byte.MIN_VALUE);
-                break;
-            case "short":
-                System.out.println("The MAX value is:" + Short.MAX_VALUE);
-                System.out.println("The MIN value is:" + Short.MIN_VALUE);
-                break;
-            case "char":
-            case "character":
-                System.out.println("The MAX value is:" + Character.MAX_VALUE);
-                System.out.println("The MIN value is:" + Character.MIN_VALUE);
-                break;
-            case "long":
-                System.out.println("The MAX value is:" + Long.MAX_VALUE);
-                System.out.println("The MIN value is:" + Long.MIN_VALUE);
-                break;
-            case "float":
-                System.out.println("The MAX value is:" + Float.MAX_VALUE);
-                System.out.println("The MIN value is:" + Float.MIN_VALUE);
-                break;
-            case "double":
-                System.out.println("The MAX value is:" + Double.MAX_VALUE);
-                System.out.println("The MIN value is:" + Double.MIN_VALUE);
-                break;
-            case "bool":
-            case "boolean":
-                System.out.println("This type of variable has only two literals False and true\nand has range about " + Byte.MAX_VALUE + " " + Byte.MIN_VALUE);
-                break;
-            case "all":
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "number", "type", "MAX length", "MIN Length");
-                System.out.println("----------------------------------------------------------------");
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "1", "integer", Integer.MAX_VALUE, Integer.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "2", "integer", Integer.MAX_VALUE, Integer.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "3", "byte", Byte.MAX_VALUE, Byte.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "4", "byte", Byte.MAX_VALUE, Byte.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "5", "short", Short.MAX_VALUE, Short.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "6", "short", Short.MAX_VALUE, Short.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "7", "char", Character.MAX_VALUE, Character.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "8", "char", Character.MAX_VALUE, Character.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "9", "long", Long.MAX_VALUE, Long.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "10", "long", Long.MAX_VALUE, Long.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "11", "float", Float.MAX_VALUE, Float.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "12", "float", Float.MAX_VALUE, Float.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "13", "double", Double.MAX_VALUE, Double.MIN_VALUE);
-                System.out.printf("%-9s%-9s%-30s%-12s%n", "14", "double", Double.MAX_VALUE, Double.MIN_VALUE);
-                break;
-            default:
-                System.out.println("Please write correct number");
-            }
+        System.out.println("Hello, " + modType);
+        System.out.println("где ти живеш?"); //pe of variable\n (for example: int, char, bool)\n Type <all> command to show result table\n Type <info> for more information");
+        String type1 = scanner.nextLine();
+        System.out.println("Ого, " + type1 + " это круто!");
     }
 }
+            /* switch (modType) {
+                case "info":
+                    System.out.println("Hello, " + modType); // this program helps to display max and min values of different type of the variables");
+                    break;
+                case "int":
+                case "integer":
+                    System.out.println("The MAX value is:" + Integer.MAX_VALUE);
+                    System.out.println("The MIN value is:" + Integer.MIN_VALUE);
+                    break;
+                case "byte":
+                    System.out.println("The MAX value is:" + Byte.MAX_VALUE);
+                    System.out.println("The MIN value is:" + Byte.MIN_VALUE);
+                    break;
+                case "short":
+                    System.out.println("The MAX value is:" + Short.MAX_VALUE);
+                    System.out.println("The MIN value is:" + Short.MIN_VALUE);
+                    break;
+                case "char":
+                case "character":
+                    System.out.println("The MAX value is:" + Character.MAX_VALUE);
+                    System.out.println("The MIN value is:" + Character.MIN_VALUE);
+                    break;
+                case "long":
+                    System.out.println("The MAX value is:" + Long.MAX_VALUE);
+                    System.out.println("The MIN value is:" + Long.MIN_VALUE);
+                    break;
+                case "float":
+                    System.out.println("The MAX value is:" + Float.MAX_VALUE);
+                    System.out.println("The MIN value is:" + Float.MIN_VALUE);
+                    break;
+                case "double":
+                    System.out.println("The MAX value is:" + Double.MAX_VALUE);
+                    System.out.println("The MIN value is:" + Double.MIN_VALUE);
+                    break;
+                case "bool":
+                case "boolean":
+                    System.out.println("This type of variable has only two literals False and true\nand has range about " + Byte.MAX_VALUE + " " + Byte.MIN_VALUE);
+                    break;
+                case "all":
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "number", "type", "MAX length", "MIN Length");
+                    System.out.println("----------------------------------------------------------------");
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "1", "integer", Integer.MAX_VALUE, Integer.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "2", "integer", Integer.MAX_VALUE, Integer.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "3", "byte", Byte.MAX_VALUE, Byte.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "4", "byte", Byte.MAX_VALUE, Byte.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "5", "short", Short.MAX_VALUE, Short.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "6", "short", Short.MAX_VALUE, Short.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "7", "char", Character.MAX_VALUE, Character.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "8", "char", Character.MAX_VALUE, Character.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "9", "long", Long.MAX_VALUE, Long.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "10", "long", Long.MAX_VALUE, Long.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "11", "float", Float.MAX_VALUE, Float.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "12", "float", Float.MAX_VALUE, Float.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "13", "double", Double.MAX_VALUE, Double.MIN_VALUE);
+                    System.out.printf("%-9s%-9s%-30s%-12s%n", "14", "double", Double.MAX_VALUE, Double.MIN_VALUE);
+                    break;
+                default:
+                    System.out.println("Please write correct number");
+
+            }
+        }
+    }*/
+
+
        /*
         ArrayList <String> values = new ArrayList <String>();
         values.add ("The MAX value is: " + Integer.MAX_VALUE);
